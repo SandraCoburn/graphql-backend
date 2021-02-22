b226b1a27395d1843d180607c234fc8ee5d546b1
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-02-21 19:43:44 -0800

    Created OrderItem and Order schemas to handle orders in cart

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1o12pwy"
+        adminMetaHash="5gkblk"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- backend/keystone.ts
+++ backend/keystone.ts
@@ -12,6 +12,8 @@ import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/bakery-sql';
@@ -62,6 +64,8 @@ export default withAuth(
       Product: Product,
       ProductImage: ProductImage,
       CartItem: CartItem,
+      OrderItem: OrderItem,
+      Order: Order,
     }),
     //Custom mutation
     extendGraphqlSchema,
