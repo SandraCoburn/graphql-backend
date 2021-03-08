f13404a2ecb9ee94ce0c8117cb2698b0068d0623
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-03-07 19:41:46 -0800

    Added permission access functions and basic asscess control via sessions

---
+++ backend/access.ts
@@ -0,0 +1,21 @@
+// At it's simplest, the access control returns a yes or no value depending on the users session
+
+import { permissionsList } from './schemas/fields';
+import { ListAccessArgs } from './types';
+
+export function isSignedIn({ session }: ListAccessArgs) {
+  return !!session;
+}
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session.data.role?.[permission];
+    },
+  ])
+);
+// Permissions check
+export const permissions = {
+  ...generatedPermissions,
+};
